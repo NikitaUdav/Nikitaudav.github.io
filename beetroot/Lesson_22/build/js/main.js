7ce122eadb40c0a5cacc00541feb4ae4298c901f
NikitaUdav <1nikitaudav@gmail.com>
2020-04-16 14:17:50 +0300

    fix

---
+++ beetroot/Lesson_22/build/js/main.js
@@ -0,0 +1,10 @@
+(function (factory) {
+	typeof define === 'function' && define.amd ? define('main', factory) :
+	factory();
+}((function () { 'use strict';
+
+
+
+})));
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7In0=
