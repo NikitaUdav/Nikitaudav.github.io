c6dacaeb662ac4b29da7443c1433e6141a1d5fe9
NikitaUdav <1nikitaudav@gmail.com>
2020-04-16 15:21:19 +0300

    try-to-fix

--- beetroot/Lesson_22/build/js/main.js
+++ beetroot/Lesson_22/build/js/main.js
@@ -1,3 +1,10 @@
-!function(n){"function"==typeof define&&define.amd?define("main",n):n()}((function(){}));
+(function (factory) {
+	typeof define === 'function' && define.amd ? define('main', factory) :
+	factory();
+}((function () { 'use strict';
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
+
+
+})));
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7In0=
--- beetroot/Lesson_22/build/js/work-1.js
+++ beetroot/Lesson_22/build/js/work-1.js
@@ -1,3 +1,16 @@
-!function(n){"function"==typeof define&&define.amd?define("work1",n):n()}((function(){"use strict";var n=prompt("Здарова! Как тебя зовут бро ?!");null===n?alert("Биджо ты все сломал!"):alert("Привет ".concat(n,"! Теперь ты бро !!!"))}));
+(function (factory) {
+  typeof define === 'function' && define.amd ? define('work1', factory) :
+  factory();
+}((function () { 'use strict';
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoid29yay0xLmpzIiwic291cmNlcyI6WyJzcmMvanMvd29yay0xLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIlwidXNlIHN0cmljdFwiO1xyXG5jb25zdCBuYW1lID0gcHJvbXB0KGDQl9C00LDRgNC+0LLQsCEg0JrQsNC6INGC0LXQsdGPINC30L7QstGD0YIg0LHRgNC+ID8hYCk7XHJcbmlmIChuYW1lID09PSBudWxsKSB7XHJcbiAgYWxlcnQoYNCR0LjQtNC20L4g0YLRiyDQstGB0LUg0YHQu9C+0LzQsNC7IWApO1xyXG59IGVsc2Uge1xyXG4gIGFsZXJ0KGDQn9GA0LjQstC10YIgJHtuYW1lfSEg0KLQtdC/0LXRgNGMINGC0Ysg0LHRgNC+ICEhIWApO1xyXG59XHJcbiJdLCJuYW1lcyI6WyJuYW1lIiwicHJvbXB0IiwiYWxlcnQiXSwibWFwcGluZ3MiOiJtR0FDQSxJQUFNQSxFQUFPQyx5Q0FDQSxPQUFURCxFQUNGRSw4QkFFQUEsdUJBQWdCRiJ9
+  var name = prompt("\u0417\u0434\u0430\u0440\u043E\u0432\u0430! \u041A\u0430\u043A \u0442\u0435\u0431\u044F \u0437\u043E\u0432\u0443\u0442 \u0431\u0440\u043E ?!");
+
+  if (name === null) {
+    alert("\u0411\u0438\u0434\u0436\u043E \u0442\u044B \u0432\u0441\u0435 \u0441\u043B\u043E\u043C\u0430\u043B!");
+  } else {
+    alert("\u041F\u0440\u0438\u0432\u0435\u0442 ".concat(name, "! \u0422\u0435\u043F\u0435\u0440\u044C \u0442\u044B \u0431\u0440\u043E !!!"));
+  }
+
+})));
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoid29yay0xLmpzIiwic291cmNlcyI6WyJzcmMvanMvd29yay0xLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIlwidXNlIHN0cmljdFwiO1xyXG5jb25zdCBuYW1lID0gcHJvbXB0KGDQl9C00LDRgNC+0LLQsCEg0JrQsNC6INGC0LXQsdGPINC30L7QstGD0YIg0LHRgNC+ID8hYCk7XHJcbmlmIChuYW1lID09PSBudWxsKSB7XHJcbiAgYWxlcnQoYNCR0LjQtNC20L4g0YLRiyDQstGB0LUg0YHQu9C+0LzQsNC7IWApO1xyXG59IGVsc2Uge1xyXG4gIGFsZXJ0KGDQn9GA0LjQstC10YIgJHtuYW1lfSEg0KLQtdC/0LXRgNGMINGC0Ysg0LHRgNC+ICEhIWApO1xyXG59XHJcbiJdLCJuYW1lcyI6WyJuYW1lIiwicHJvbXB0IiwiYWxlcnQiXSwibWFwcGluZ3MiOiI7Ozs7O0VBQ0EsSUFBTUEsSUFBSSxHQUFHQyxNQUFNLGdKQUFuQjs7RUFDQSxJQUFJRCxJQUFJLEtBQUssSUFBYixFQUFtQjtFQUNqQkUsRUFBQUEsS0FBSyx3R0FBTDtFQUNELENBRkQsTUFFTztFQUNMQSxFQUFBQSxLQUFLLGdEQUFXRixJQUFYLGdGQUFMO0VBQ0Q7Ozs7In0=
