599d276bebd3f143221db4b88961420942648d5c
NikitaUdav <1nikitaudav@gmail.com>
2020-05-20 16:55:23 +0300

    fix

--- beetroot/Lesson_slider/src/js/glide.js
+++ beetroot/Lesson_slider/src/js/glide.js
@@ -1,3 +1,22 @@
 "use strict";
 import Glide from "@glidejs/glide";
-new Glide(".glide", { type: "carousel", perView: 4, autoplay: 3000 }).mount();
+const config = {
+  type: "carousel",
+  startAt: 1,
+  perView: 4,
+  focusAt: 1,
+  gap: 45,
+  animationDuration: 500,
+  breakpoints: {
+    1300: {
+      perView: 3,
+    },
+    900: {
+      perView: 2,
+    },
+    600: {
+      perView: 1,
+    },
+  },
+};
+new Glide(".glide", config).mount();
